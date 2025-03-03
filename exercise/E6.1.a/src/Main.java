7315c439737afa8c07ceb60dcff60f17d56a9448
amirreza-shahbazi2 <shbazy02@gmail.com>
2025-03-03 12:52:53 -0800

    E6.1.a

---
+++ exercise/E6.1.a/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int sum = 0;
+        for (int i = 2; i <= 100; i++)
+            if (i % 2 == 0) {
+                sum += i;
+            }
+        System.out.println("sum of all numbers between 2 and 100: " + sum);
+    }
+}
