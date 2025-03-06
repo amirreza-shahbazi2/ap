05c529a4649e75f7c6b469319a842b01470450e2
amirreza-shahbazi2 <shbazy02@gmail.com>
2025-03-06 13:45:28 -0800

    E6.9

---
+++ exercise/E6.9/src/Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a word: ");
+        String word = scanner.nextLine();
+        String reversed = "";
+        for (int i = word.length() - 1; i >= 0; i--) {
+            reversed += word.charAt(i);
+        }
+        System.out.println(reversed);
+        scanner.close();
+    }
+}
