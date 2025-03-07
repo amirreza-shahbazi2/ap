3a158bee75bcf0230ecdee499e6bf60567bda96c
amirreza-shahbazi2 <shbazy02@gmail.com>
2025-03-07 02:30:57 -0800

    E6.13

---
+++ exercise/E6.13/src/Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+        if (number == 0) {
+            System.out.println("0");
+        } else {
+            while (number > 0) {
+                int digit = number % 2;
+                System.out.println(digit);
+                number /= 2;
+            }
+        }
+        scanner.close();
+    }
+}
