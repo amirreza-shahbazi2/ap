a500bcecbdea8ab3d26ed251ec1d0a3a047a508b
amirreza-shahbazi2 <shbazy02@gmail.com>
2025-03-25 07:46:53 -0700

    EX2_PM_1_2

---
+++ exercises/ex2/untitled/src/Main_EX2_PM_1_2.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main_EX2_PM_1_2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number: ");
+        int k = sc.nextInt();
+        char[][] board= new char[k + 2][k + 2];
+
+        for(int i=0; i<k+2; i++) {
+            for (int j = 0; j < k+2; j++) {
+                if (i == 0 || i == k+1 || j == 0 || j==k+1) {
+                    board[i][j] = '*';
+                } else {
+                    board[i][j] = ' ';
+                }
+            }
+        }
+        for(int i=0; i<k+2; i++) {
+            for(int j=0; j<k+2; j++) {
+                System.out.print(board[i][j]);
+            }
+            System.out.println();
+        }
+    }
+}
