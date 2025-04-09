b530937b1e664a51f2cf3e2eca170e00f23cfe12
amirreza-shahbazi2 <shbazy02@gmail.com>
2025-04-09 13:08:39 -0700

    EX3_LM_1_3

---
+++ exercises/EX3/Main_EX3_LM_1_3.java
@@ -0,0 +1,32 @@
+package exercises.EX3;
+
+import exercises.ex5.Student;
+
+public class Main_EX3_LM_1_3 {
+    public static Student searchbyname(String firstname, String lastname, Student[] students){
+        for (Student student : students) {
+            if (student.getFirstname().equalsIgnoreCase(firstname) &&
+                    student.getLastname().equalsIgnoreCase(lastname)) {
+                return student;
+            }
+        }
+        return null;
+    }
+
+    public static void main(String[] args) {
+        Student[] students = {
+                new Student("reza", "rezaei", "967543761", "computer science"),
+                new Student("ali", "shahbazi", "254859714", "electrical engineering")
+        };
+        Student F=searchbyname("reza","rezaei",students);
+        if(F!=null){
+            System.out.println("student found \n"+F.getFirstname()+ F.getLastname());
+        }
+        else{
+            System.out.println("student not found");
+        }
+
+
+    }
+
+}
